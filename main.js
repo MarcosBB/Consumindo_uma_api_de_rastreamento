f3835fc2d7348aa65e30ba81c15215ba20bb2e25
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-06-28 11:37:08 -0400

    melhorando pesquisa

--- main.js
+++ main.js
@@ -1,33 +1,68 @@
 
 const url = 'https://rastreamento-api.herokuapp.com/busca-atualizacoes/'
 
-function adicionaColuna(tabela, content){
+function adicionaColuna(tabela, line, content){
     var item
-    item = document.createElement("td")
+    item = line.insertCell()
     item.innerText = content
     tabela.appendChild(item)
 }
 
+function adicionaLinhas(tabela){
+    linhas = ["Status", "Rotas", "Data"]
+    
+    var item
+    for (linha in linhas){
+        item = document.createElement("th")
+        item.innerText = linhas[linha]
+        tabela.appendChild(item)
+    }
+}
+
 function pesquisar(id){
+    var input = document.getElementById("id")
+    var id = input.value
+
+    if(id == ""){
+        console.log("Digite o id do seu produto")
+        var item
+        item = document.getElementById("status")
+        item.innerText = "Digite o id do seu produto"
+    }
+    else{
     fetch(url + "last/" + id + "/")
         .then(response => response.json())
         .then(data => {
             console.log(data)
 
             if (data.status == "Busca bem sucedida"){
-            const status = data.resultado.status
-            const date = data.resultado.date
-            const rota = data.resultado.rota
+                const status = data.resultado.status
+                const date = data.resultado.date
+                const rota = data.resultado.rota
 
-            const tabela = document.getElementById("resultados")
+                item = document.getElementById("status")
+                item.innerText = "Resultado encontrado"
 
-            document.createElement("tr")
+                const tabela = document.getElementById("resultados")
 
-            adicionaColuna(tabela, status)
-            adicionaColuna(tabela, rota)
-            adicionaColuna(tabela, date)
+                
+
+                adicionaLinhas(tabela)
+
+                var line = tabela.insertRow()
+
+                adicionaColuna(tabela,line, status)
+                adicionaColuna(tabela,line, rota)
+                adicionaColuna(tabela,line, date)
+            }
+            else if (data.status == "Busca mal sucedida"){
+                console.log("Nenhum resultado encontrado")
+                var item
+                item = document.getElementById("status")
+                item.innerText = "Nenhum resultado encontrado"
             }
         })
+    }
 }
 
 function clickDoBotao(){
